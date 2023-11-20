a2863c3bfdf3ef4480d2762ea7816016edfa3887
jhon-p16 <jhonpaulo1698@gmail.com>
2023-11-19 21:00:01 -0500

    subir proyecto

---
+++ src/clases/Especialidad.java
@@ -0,0 +1,16 @@
+package clases;
+
+/**
+ * Enum con los datos de las especialidades de los médicos
+ * @author Jhon
+ */
+public enum Especialidad {
+    Oftalmólogo,
+    Neurólogo,
+    Dermatólogo,
+    Traumatólogo,
+    Ginecólogo,
+    Urólogo,
+    Cardiólogo,
+    General
+}
